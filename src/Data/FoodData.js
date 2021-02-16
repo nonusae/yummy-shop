af372b12c08aea53becf7007ee785bbc22c80289
nonusae <nonusae@gmail.com>
2021-02-16 23:14:31 +0700

    Add food sections

--- src/Data/FoodData.js
+++ src/Data/FoodData.js
@@ -1,30 +1,48 @@
-export const foods = [
+const foodItems = [
   {
     name: 'Cheese Pizza',
     img: '/img/pizza.png',
+    section: 'Pizza'
   },
   {
     name: 'Pepperoni Pizza',
     img: '/img/pizza2.jpeg',
+    section: 'Pizza'
+
   },
   {
     name: 'Chicken Pizza',
     img: '/img/chicken-pizza.jpeg',
+    section: 'Pizza'
+
   },
   {
     name: 'Veggie Pizza',
     img: '/img/healthy-pizza.jpeg',
+    section: 'Pizza'
   },
   {
     name: 'Burger',
     img: '/img/burger.jpeg',
+    section: 'Sandwich'
   },
   {
     name: 'Shirmp PoBoy',
     img: '/img/sandwich.jpeg',
+    section: 'Sandwich'
   },
   {
     name: 'Fries',
     img: '/img/fries.jpeg',
+    section: 'Fries'
   },
 ]
+
+export const foods = foodItems.reduce((res, food) => {
+  if(!res[food.section]) {
+    res[food.section] = [];
+  }
+
+  res[food.section].push(food);
+  return res;
+}, {});
