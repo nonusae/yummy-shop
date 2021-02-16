b83dc06ac9b1b5662a9b0ca237d32c31889600e6
nonusae <nonusae@gmail.com>
2021-02-16 23:38:08 +0700

    Clean up App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
-import { createGlobalStyle } from 'styled-components';
 import { NavBar } from './NavBar/NavBar';
 import { Banner } from './Banner/Banner';
 import { Menu } from './Menu/Menu'
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    font-family: 'Open Sans', sans-serif;
-    margin: 0;
-  }
-
-  h1, h2, h3 {
-    font-family: 'Righteous', cursive;
-  }
-`
+import { GlobalStyle } from './styles/GlobalStyle'
 
 function App() {
   return (
@@ -21,7 +10,6 @@ function App() {
       <NavBar />
       <Banner />
       <Menu />
-      <div>Hello</div>
     </>
   );
 }
