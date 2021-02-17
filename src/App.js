14c7cdf41b02facc022f0271ea3120d3c0b271ca
nonusae <nonusae@gmail.com>
2021-02-17 19:41:52 +0700

    Add FoodDialog

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
+import React, { useState } from 'react';
 import { NavBar } from './NavBar/NavBar';
 import { Banner } from './Banner/Banner';
 import { Menu } from './Menu/Menu'
-import { GlobalStyle } from './styles/GlobalStyle'
+import { FoodDialog } from './FoodDialog/FoodDialog'
+import { GlobalStyle } from './styles/GlobalStyle';
 
 function App() {
+  const [openFood, setOpenFood] = useState();
+
   return (
     <>
       <GlobalStyle />
+      <FoodDialog openFood={openFood} setOpenFood={setOpenFood}/>
       <NavBar />
       <Banner />
-      <Menu />
+      <Menu setOpenFood={setOpenFood} />
     </>
   );
 }
